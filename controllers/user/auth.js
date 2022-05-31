f6054bcbf9c440751493c2c6f6e617cfc0cc43a0
seyicoded <opadonuseyi01@gmail.com>
2022-05-31 12:53:43 +0100

    sms fix $2

--- controllers/user/auth.js
+++ controllers/user/auth.js
@@ -286,7 +286,7 @@ const requestOtpController = async(req, res)=>{
                 // send otp to both email and phone
                 (async()=>{
                     sendMail(email, "OTP From Reasns", message);
-                    sendSMS({phone, message});
+                    sendSMS({to: phone, body: message});
                 })()
 
                 db.execute("UPDATE otp_session SET otp = ?, expire = ?, status = ? WHERE phone = ? AND email = ?", [code, expire, status, phone, email], (err1, results1, fields1)=>{
