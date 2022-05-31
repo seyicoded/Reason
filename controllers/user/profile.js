847b5ddd822397ed1e080a17809b93c4d2a71b74
seyicoded <opadonuseyi01@gmail.com>
2022-05-31 16:14:05 +0100

    integration of email #2

--- controllers/user/profile.js
+++ controllers/user/profile.js
@@ -22,47 +22,109 @@ const uploadFile = async(path, filename) => {
 
 const uploadMainMediaController = async (req, res)=>{
     const form = formidable({ multiples: true });
+    const user_id = (await req.user).u_id;
 
-    form.parse(req, async (err, fields, files) => {
-        const media = (files.media);
-        let originalFilename = media.originalFilename;
-        let originalFilenameArr = originalFilename.split('.')
-        let fileExtension = originalFilenameArr[originalFilenameArr.length - 1]
-        originalFilename = `${uuidv4()}.${fileExtension}`;
+    // check if main profile already exist
+    db.execute("SELECT * FROM users_image WHERE u_id = ?", [user_id], (err, results)=>{
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'an error occurred',
+                error: err
+            })
+        }
 
-        const user_id = (await req.user).u_id;
-
-        const url = await uploadFile(`${media.filepath}`, originalFilename)
-
-        // console.log(user_id)
-        // console.log(url)
-        // return
-
-        db.execute("INSERT INTO users_images(id, u_id, image, status) VALUES(?, ?, ?, ?)",[uuidv4(), user_id, url, 1],(err1, results, fields)=>{
-            if(err1){
+        if(results.length > 0){
+            if(results[0].main_media){
                 return res.status(500).json({
                     status: false,
-                    message: 'an error occurred',
-                    error: err
-                })
-            }
-
-            if(results.affectedRows > 0){
-                return res.status(201).json({
-                    status: true,
-                    message: 'Main media successfully uploaded',
-                    results: results,
-                    url: url
-                })
-            }else{
-                return res.status(500).json({
-                    status: false,
-                    message: 'an error occurred',
+                    message: 'Main media already exist',
                     error: 'error'
                 })
             }
-        })
+
+            form.parse(req, async (err2, fields, files)=>{
+                if(err2){
+                    return res.status(500).json({
+                        status: false,
+                        message: 'an error occurred',
+                        error: err
+                    })
+                }
+    
+                const media = (files.media);
+                let originalFilename = media.originalFilename;
+                let originalFilenameArr = originalFilename.split('.')
+                let fileExtension = originalFilenameArr[originalFilenameArr.length - 1]
+                originalFilename = `${uuidv4()}.${fileExtension}`;
+        
+                const url = await uploadFile(`${media.filepath}`, originalFilename)
+    
+                db.execute("UPDATE users_image SET image = ? WHERE u_id = ?", [url, user_id], (err, results)=>{
+                    if(err){
+                        return res.status(500).json({
+                            status: false,
+                            message: 'an error occurred',
+                            error: err
+                        })
+                    }
+    
+                    if(results.affectedRows > 0){
+                        return res.status(201).json({
+                            status: true,
+                            message: 'Main media successfully uploaded',
+                            results: results
+                        })
+                    }else{
+                        return res.status(500).json({
+                            status: false,
+                            message: 'an error occurred',
+                            error: 'error'
+                        })
+                    }
+                })
+            })
+        }else{
+            form.parse(req, async (err, fields, files) => {
+                const media = (files.media);
+                let originalFilename = media.originalFilename;
+                let originalFilenameArr = originalFilename.split('.')
+                let fileExtension = originalFilenameArr[originalFilenameArr.length - 1]
+                originalFilename = `${uuidv4()}.${fileExtension}`;
+        
+                const url = await uploadFile(`${media.filepath}`, originalFilename)
+        
+                db.execute("INSERT INTO users_images(id, u_id, image, status) VALUES(?, ?, ?, ?)",[uuidv4(), user_id, url, 1],(err1, results, fields)=>{
+                    if(err1){
+                        return res.status(500).json({
+                            status: false,
+                            message: 'an error occurred',
+                            error: err
+                        })
+                    }
+        
+                    if(results.affectedRows > 0){
+                        return res.status(201).json({
+                            status: true,
+                            message: 'Main media successfully uploaded',
+                            results: results,
+                            url: url
+                        })
+                    }else{
+                        return res.status(500).json({
+                            status: false,
+                            message: 'an error occurred',
+                            error: 'error'
+                        })
+                    }
+                })
+            })
+        }
+
+        
     })
+
+    
 }
 
 module.exports = {
