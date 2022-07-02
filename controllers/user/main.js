345b9758134f2c0701eb60996ca7b3eaf8d96388
seyicoded <opadonuseyi01@gmail.com>
2022-07-02 18:48:24 +0100

    get sent like reqeust list

--- controllers/user/main.js
+++ controllers/user/main.js
@@ -157,7 +157,13 @@ const getActiveLikeRequestController = async (req, res)=>{
     try{
         const user_id = (await req.user).u_id;
 
+        const data = await db.promise().query("SELECT * FROM like_linker WHERE reciever_id = ? AND STATUS = ?", [user_id, 0])[0];
 
+        return res.status(200).json({
+            status: true,
+            data: data,
+            message: 'Successfully fetched',
+        })
     }catch(e){
         return res.status(200).json({
             status: false,
