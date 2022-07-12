ec1ea5e55d5180a1f9c80fafbb00efa2d31521dc
seyicoded <opadonuseyi01@gmail.com>
2022-07-12 01:09:53 +0100

    fill marriage form

--- controllers/user/marriage.js
+++ controllers/user/marriage.js
@@ -48,6 +48,90 @@ const getMarriageRegStatus = async (req, res)=>{
     }
 }
 
+const fillForm = async (req, res)=>{
+    // check if marriage entry exist, if not, then return marriage entry not entered
+    // if it exist, check if it's completely filled
+
+    try {
+        const user_id = (await req.user).u_id;
+
+        const {
+            is_anonymous,
+            marital_status,
+            pm_country,
+            pm_state,
+            dob,
+            has_kids,
+            kids,
+            want_kids,
+            accept_other_kids,
+            could_adopt,
+            family_type,
+            family_size,
+            accomodate_other_family,
+            raised_by,
+            partner_contribution_preference,
+            salary_range,
+            partner_height,
+            domestic_work,
+            health_condition,
+            blood_group,
+            genotype
+        } = req.body
+
+        const marriage = (await db.promise().query("SELECT * FROM marriage WHERE u_id = ?", [user_id]))[0]
+
+        let msg = '';
+
+        if(marriage.length > 0){
+            // check if data is already created, if so, update it
+
+            let sql = "UPDATE marriage SET is_anonymous = ?, marital_status = ?, pm_country = ?, pm_state = ?, dob = ?, has_kids = ?, kids = ?, want_kids = ?, accept_other_kids = ?, could_adopt = ?, family_type = ?, family_size = ?, accomodate_other_family = ?, raised_by = ?, partner_contribution_preference = ?, salary_range = ?, partner_height = ?, domestic_work = ?, health_condition = ?, blood_group = ?, genotype = ? WHERE u_id = ?";
+            const res = (await db.promise().query(sql, [is_anonymous, marital_status, pm_country, pm_state, dob, has_kids, kids, want_kids, accept_other_kids, could_adopt, family_type, family_size, accomodate_other_family, raised_by, partner_contribution_preference, salary_range, partner_height, domestic_work, health_condition, blood_group, genotype, user_id]))[0];
+
+            if(res.affectedRows == 1){
+                return res.status(200).json({
+                    status: true,
+                    message: 'Form filled successfully',
+                })
+            }else{
+                return res.status(200).json({
+                    status: false,
+                    message: 'An error occurred'
+                })
+            }
+        }else{
+            // create data
+
+            // affectedRows = 0 if already exist, 1 if not exist
+
+            let sql = "INSERT INTO `marriage` (`u_id`, `is_anonymous`, `marital_status`, `pm_country`, `pm_state`, `dob`, `has_kids`, `kids`, `want_kids`, `accept_other_kids`, `could_adopt`, `family_type`, `family_size`, `accomodate_other_family`, `raised_by`, `partner_contribution_preference`, `salary_range`, `partner_height`, `domestic_work`, `health_condition`, `blood_group`, `genotype`, `status`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            let params = [user_id, is_anonymous, marital_status, pm_country, pm_state, dob, has_kids, kids, want_kids, accept_other_kids, could_adopt, family_type, family_size, accomodate_other_family, raised_by, partner_contribution_preference, salary_range, partner_height, domestic_work, health_condition, blood_group, genotype, 1];
+            const res = (await db.promise().query(sql, params))[0];
+
+            if(res.affectedRows == 1){
+                return res.status(200).json({
+                    status: true,
+                    message: 'Form filled successfully',
+                })
+            }else{
+                return res.status(200).json({
+                    status: false,
+                    message: 'An error occurred'
+                })
+            }
+        }
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            status: false,
+            message: 'An error occurred',
+            data: e,
+        })
+    }
+}
+
 module.exports = {
-    getMarriageRegStatus
+    getMarriageRegStatus,
+    fillForm
 }
--- routes/user/marriage.js
+++ routes/user/marriage.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express.Router();
 const {
-    getMarriageRegStatus} =  require('../../controllers/user/marriage')
+    getMarriageRegStatus,
+    fillForm} =  require('../../controllers/user/marriage')
 
 const {
     verifyUsersToken
     } = require('../../middlewares/authvet');
 
 app.get('/marriage/reg-status', verifyUsersToken, getMarriageRegStatus)
+app.post('/marriage/form', verifyUsersToken, fillForm)
 // app.get('/marriage', verifyUsersToken, getSubscription)
 // get
 // update-location
